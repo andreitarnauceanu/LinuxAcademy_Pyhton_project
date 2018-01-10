4ae392c1e4c1ab7c88825688a0d6e27093fd904b
Andrei Tarnauceanu <linuxacademystudent@gmail.com>
2018-01-10 02:21:24 +0000

    Implement CLI parser

---
+++ tests/test_cli.py
@@ -0,0 +1,50 @@
+import pytest
+
+from pgbackup import cli
+
+url = "postgres://bob:password@example.com:5432/db_one"
+
+def test_parser_without_driver():
+    """
+    Without a specified driver the parser will exit
+    """
+    parser = cli.create_parser()
+    with pytest.raises(SystemExit):
+        parser.parse_args([url])
+
+def test_parser_with_driver():
+    """
+    The parser will exit if it receives a driver without a
+    destination.
+    """
+    parser = cli.create_parser()
+    with pytest.raises(SystemExit):
+        parser.parse_args([url, '--driver', 'local'])
+
+def test_parser_with_driver_and_destination():
+    """
+    The parser will not exit if it receives a driver and
+    a destination
+    """
+    parser = cli.create_parser()
+    args= parser.parse_args([url, '--driver', 'local', '/some/path'])
+
+    assert args.driver == 'local'
+    assert args.destination == '/some/path'
+
+def test_parser_with_unknown_driver(parser):
+    """
+    The parser will exit if driver name is unknown.
+    """
+    parser = cli.create_parser()
+    with pytest.raises(SystemExit):
+        parser.parse_args([url, '--driver', 'azure', 'destination'])
+
+def test_parser_with_known_drivers(parser):
+    """
+    The parser will not exit if the driver name is known.
+    """
+    parser = cli.create_parser()
+    for driver in ['local', 's3']:
+        assert parser.parse_args([url, '--driver', driver, 'destination'])
+
